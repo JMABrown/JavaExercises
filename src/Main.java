ed7ed2f6641580f9f895829cfeab8dfa3a8ab610
James Brown <james.mich.alex.brown@gmail.com>
2026-03-24 09:13:53 +0000

    Added week 2 method challenges

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 import java.io.Console;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayListChallenges.Challenge3Extended();
+        MethodChallenges.TestGetOdds();
     }
 }
